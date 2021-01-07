c131af7c1e3b4949a39e83f3f87a9d76c8498733
Charles Doebler <cdoebler1@gmail.com>
2021-01-06 20:19:10 -0500

    added monitoring routine

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 import subprocess
+from threading import Thread
+from time import sleep
 
 
 class fcr_reporting(MycroftSkill):
@@ -25,12 +27,34 @@ class fcr_reporting(MycroftSkill):
     def winston_power(self, message):
         p = 'echo "get battery" | nc -q 0 127.0.0.1 8423'
         pwr = subprocess.Popen(['sudo', 'sh', '-c', p], stdout=subprocess.PIPE)
-        pwr = int(pwr.communicate()[0].decode('ascii')[8:-7].strip())
+        pwr = pwr.communicate()[0].decode('ascii')[8:-7].strip()
         self.speak("I am at {} percent power.".format(pwr))
 
     def stop(self):
         pass
 
 
+class fcr_monitoring(MycroftSkill, Thread):
+    def __init__(self):
+        Thread.MycroftSkill.__init__(self)
+
+    def winston_monitor_temp(self, message):
+        while True:
+            temp = subprocess.Popen(['/opt/vc/bin/vcgencmd', 'measure_temp'],
+                                    stdout=subprocess.PIPE)
+            temp = int(temp.communicate()[0].decode('ascii')[5:-3])
+            if temp > 90:
+                self.speak("Core temperature is critical."
+                           "Powering off immediately to mitigate damage.")
+            elif temp > 80:
+                self.speak("Core temperature exceeds 80 degrees celsius."
+                           "Throttling core speed to reduce temperature.")
+            elif temp > 70:
+                self.speak("I am getting warm. My current core temperature is"
+                           "{} degrees celsius.")
+        sleep(30)
+
+
 def create_skill():
     return fcr_reporting()
+    return fcr_monitoring()
